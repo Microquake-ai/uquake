90f68c0cb9c64a680dd56b2fae7661a3b71c35e3
jpmercier <jpmercier01@gmail.com>
2023-11-01 15:20:45 -0400

    cleaning the test directory

--- tests/test_stationxml.py
+++ tests/test_stationxml.py
@@ -1,47 +1,62 @@
+import unittest
 from uquake.core import inventory
 from uquake.core.coordinates import Coordinates
-from importlib import reload
 import random
 import numpy as np
-reload(inventory)
+import os
 
-stations = []
-for i in range(0, 10):
-    channels = []
-    for channel_code in ['x', 'y', 'z']:
-        x, y, z = [random.randint(0, 1000) for i in range(0, 3)]
-        location_code = '00'
-        orientation_vector = [random.randint(0, 1000) for i in range(0, 3)]
-        orientation_vector = orientation_vector / np.linalg.norm(orientation_vector)
 
-        if random.choice([True, False]):
-            system_response = inventory.SystemResponse('geophone',
-                                                       resonance_frequency=15,
-                                                       gain=47)
-        else:
-            system_response = inventory.SystemResponse('accelerometer',
-                                                       resonance_frequency=2300,
-                                                       gain=100,
-                                                       sensitivity=0.2)
+class TestInventoryMethods(unittest.TestCase):
 
-        system_response.add_cable(output_resistance=2500, cable_length=1000,
-                                  cable_capacitance=1e-12)
+    def tearDown(self):
+        # Remove the test.xml file after the test
+        if os.path.exists('test.xml'):
+            os.remove('test.xml')
 
-        channels.append(inventory.Channel(channel_code, location_code,
-                                          coordinates=Coordinates(x, y, z),
-                                          orientation_vector=orientation_vector,
-                                          response=system_response.response))
-    station_code = f'STA{i:02d}'
-    x, y, z = [random.randint(0, 1000) for i in range(0, 3)]
-    stations.append(inventory.Station(station_code, coordinates=Coordinates(x, y, z),
-                                      channels=channels))
+    def test_inventory_serialization(self):
+        stations = []
+        for i in range(10):
+            channels = []
+            for channel_code in ['x', 'y', 'z']:
+                x, y, z = [random.randint(0, 1000) for _ in range(3)]
+                location_code = '00'
+                orientation_vector = np.array([random.randint(0, 1000) for _ in
+                                               range(3)]).astype(float)
+                orientation_vector /= np.linalg.norm(orientation_vector)
 
-network = inventory.Network(code='XX', stations=stations)
-inv = inventory.Inventory(networks=[network])
+                if random.choice([True, False]):
+                    system_response = inventory.SystemResponse('geophone',
+                                                               resonance_frequency=15,
+                                                               gain=47)
+                else:
+                    system_response = inventory.SystemResponse('accelerometer',
+                                                               resonance_frequency=2300,
+                                                               gain=100,
+                                                               sensitivity=0.2)
 
-# reading and writing and checking that the object is the same before and after
+                system_response.add_cable(output_resistance=2500, cable_length=1000,
+                                          cable_capacitance=1e-12)
 
-inv.write('test.xml')
+                channels.append(inventory.Channel(channel_code, location_code,
+                                                  coordinates=Coordinates(x, y, z),
+                                                  orientation_vector=orientation_vector,
+                                                  response=system_response.response))
+            station_code = f'STA{i:02d}'
+            x, y, z = [random.randint(0, 1000) for _ in range(3)]
+            stations.append(inventory.Station(station_code,
+                                              coordinates=Coordinates(x, y, z),
+                                              channels=channels))
 
-inv2 = inventory.read_inventory('test.xml')
+        network = inventory.Network(code='XX', stations=stations)
+        inv = inventory.Inventory(networks=[network])
 
+        # Write and read back
+        inv.write('test.xml')
+        inv2 = inventory.read_inventory('test.xml')
+
+        # Check equality
+        self.assertEqual(inv, inv2)
+
+
+if __name__ == '__main__':
+    unittest.main()
