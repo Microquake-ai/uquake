762fd500c2fa49a9b66878c1371c0222d85814ca
jpmercier <jpmercier01@gmail.com>
2023-11-23 17:12:45 -0500

    nc

--- uquake/grid/extended.py
+++ uquake/grid/extended.py
@@ -53,8 +53,6 @@ from uquake.synthetic.inventory import generate_unique_instrument_code
 from uquake.core.event import ResourceIdentifier
 from .base import __default_grid_label__
 from typing import Union, Tuple
-from uquake.io.grid.nlloc import (write_grid_data, write_grid_model_id,
-                                  write_grid_header)
 
 __cpu_count__ = cpu_count()
 
