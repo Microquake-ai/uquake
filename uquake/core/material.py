d25f1c46ad5b5fb9f737e9c92137ee7639397428
Jean-Philippe Mercier <jpmercier@museis.ai>
2025-02-26 10:50:33 -0500

    fixing an issue

--- uquake/core/material.py
+++ uquake/core/material.py
@@ -768,7 +768,9 @@ class Device(BaseModel):
         # correct for the device azimuth
         azimuth = azimuth % 360
         if self.device_type.coordinate_system != coordinates.coordinate_system:
-            self.change_components_coordinate_system(self.coordinates.coordinate_system)
+            self.device_type.change_components_coordinate_system(
+                self.coordinates.coordinate_system
+            )
         self.device_type.rotate_components_azimuth(azimuth)
 
         channels = []
