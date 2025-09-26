fd7473f222a162d16ac511d33fbf3d522ec35baf
Jean-Philippe Mercier <jpmercier@caurtech.com>
2025-09-26 15:30:24 -0400

    updating the compute_frechet_eikonal function

--- uquake/grid/extended.py
+++ uquake/grid/extended.py
@@ -3703,15 +3703,15 @@ class PhaseVelocity(Grid):
             raise ValueError("Velocity grid must be strictly positive for eikonal computations.")
 
         if self.grid_type == GridTypes.VELOCITY_KILOMETERS and self.grid_units == GridUnits.METER:
-            velocity = velocity_data * 1e3  # convert to m/s
+            velocity = velocity_data.copy() * 1e3  # convert to m/s
 
         elif self.grid_type == GridTypes.VELOCITY_METERS and self.grid_units == GridUnits.KILOMETER:
-            velocity = velocity_data * 1e-3  # convert to km/s
+            velocity = velocity_data.copy() * 1e-3  # convert to km/s
 
         else:
             raise ValueError("Grid type not supported")
 
-        velocity_grid = EKImageData(velocity_data.copy(), origin=tuple(origin_vec), spacing=spacing_value)
+        velocity_grid = EKImageData(velocity, origin=tuple(origin_vec), spacing=spacing_value)
 
         # unique_src_grid = self.transform_to_grid(unique_src_coords)
         # unique_rcv_grid = self.transform_to_grid(unique_rcv_coords)
